64c3948decb9c6cc6891ecc1bcbadbfe730c0e1d
佐藤翼 <hireniku.smile@i.softbank.jp>
2018-12-26 17:51:49 +0900

    Initial Commit

---
+++ Dietsupporter1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dietsupporter1
+//
+//  Created by 佐藤翼 on 2018/12/26.
+//  Copyright © 2018 Tsubasa_Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
