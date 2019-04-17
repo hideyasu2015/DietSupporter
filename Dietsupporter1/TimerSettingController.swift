5c38841adf7373c23ea5e9b67ed2833b34cd31ff
佐藤翼 <hireniku.smile@i.softbank.jp>
2019-04-17 11:39:02 +0900

    TimerSettingControllerを編集。Timersオブジェクトを生成。

--- Dietsupporter1/TimerSettingController.swift
+++ Dietsupporter1/TimerSettingController.swift
@@ -9,26 +9,41 @@
 import Foundation
 import UIKit
 class TimerSettingController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func textlabel_name(_ sender: Any) {
-    }
+    @IBOutlet weak var menu_name: UITextField!
     
-    @IBAction func textLabel_exerciseTime(_ sender: Any) {
-    }
+    @IBOutlet weak var exercise_time: UITextField!
     
-    @IBAction func textField_breakTime(_ sender: Any) {
-    }
+    @IBOutlet weak var rest_time: UITextField!
     
-    @IBAction func textField_set(_ sender: Any) {
-    }
+    @IBOutlet weak var set_count: UITextField!
     
     @IBAction func btn_registration(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let timer = Timers(context: context)
+        timer.name = menu_name.text
+        timer.exercise_time = Int64(exercise_time!.text!) ?? 0
+        timer.rest_time = Int64(rest_time!.text!) ?? 0
+        timer.set_count = Int64(set_count!.text!) ?? 0
+        
+        do {
+            try context.save()
+        }catch{
+            print(error)
+        }
     }
     
-    
-    
-    
-}
+    class func dateFromString(string: String, format: String) -> Date{
+        let formatter:DateFormatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.dateFormat = format
+        return formatter.date(from: string) ?? Date()
+    }
+    }
